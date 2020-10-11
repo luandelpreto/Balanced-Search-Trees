53e135b087c3ee99e4b3df9602ef4584d3883dfb
Luan <31374344+luandelpreto@users.noreply.github.com>
2020-10-11 16:32:23 -0400

    Update Lor_assert.c

--- common/Lor_assert.c
+++ common/Lor_assert.c
@@ -5,7 +5,7 @@
 
 void Lor_assert_abort(const char *restrict, const char *restrict, unsigned long);
 
-void (Lor_assert)(int e, const char *reason)
+void (Lor_assert)(int e, const char *restrict reason)
 {
     Lor_assert(e, reason);
 }
